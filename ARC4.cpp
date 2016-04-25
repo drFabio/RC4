46d555c07ad7c94be787c1274e8903a610787333
fabio <blackjackdevel@gmail.com>
2016-04-25 10:35:08 -0300

    RC4 implementation

---
+++ ARC4.cpp
@@ -0,0 +1,58 @@
+#include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include "ARC4.h"
+#include <iostream>
+
+using namespace std;
+
+void  ARC4::prga(unsigned char * plaintext,unsigned char * cipher,int size){
+    for(int k=0;k<size;k++){
+        prgaIndexA=(prgaIndexA+1)%256;
+        prgaIndexB=(prgaIndexB+sbox[prgaIndexA])%256;
+        swap(sbox,prgaIndexA,prgaIndexB);
+        cipher[k]=sbox[(sbox[prgaIndexA]+sbox[prgaIndexB])%256]^plaintext[k];
+    }
+}
+
+void  ARC4::prga_char( char * plaintext, char * cipher,int size){
+    for(int k=0;k<size;k++){
+        prgaIndexA=(prgaIndexA+1)%256;
+        prgaIndexB=(prgaIndexB+sbox[prgaIndexA])%256;
+        swap(sbox,prgaIndexA,prgaIndexB);
+        cipher[k]=sbox[(sbox[prgaIndexA]+sbox[prgaIndexB])%256]^plaintext[k];
+    }
+}
+void ARC4::hello() {
+  std::cout << "Hello World! ";
+} 
+void ARC4::encrypt_char( char * plaintext, char * ciphertext,int size){
+    prga_char(plaintext,ciphertext,size);
+}
+void ARC4::encrypt(unsigned char * plaintext,unsigned char * ciphertext,int size){
+    prga(plaintext,ciphertext,size);
+}
+ARC4::ARC4(){
+}
+void ARC4::swap(unsigned char data[],int i ,int j){
+    unsigned char temp=data[i];
+    data[i]=data[j];
+    data[j]=temp;
+}
+void ARC4::ksa(){
+    int j=0;
+    for(int i=0;i<256;i++){
+        j=(j+sbox[i]+key[i% sizeKey])%256;
+        swap(sbox,i,j);
+    }
+}
+void ARC4::set_key(unsigned char  k[],int size){
+    key=k;
+    prgaIndexA=0;
+    prgaIndexB=0;
+    sizeKey=size;
+    for(int i=0;i<256;i++){
+        sbox[i]=(unsigned char)i;
+    }
+    ksa();
+}
---
+++ ARC4.h
@@ -0,0 +1,28 @@
+/*
+ * ARC4.h
+ *
+ *  Created on: Nov 9, 2011
+ *      Author: fabio
+ */
+#ifndef ARC4_H_
+    #define ARC4_H_
+    class ARC4{
+        public:
+            void set_key(unsigned char * k,int size);
+            void encrypt(unsigned char * in,unsigned char * out,int size);
+            void encrypt_char(char * in,char * out,int size);
+            void ksa();
+            static void hello();
+            ARC4();
+        protected:
+            void swap(unsigned char data[],int i ,int j);
+            void prga(unsigned char * plaintext,unsigned char * cipher,int size);
+            void prga_char(char * plaintext,char * cipher,int size);
+            unsigned char * key;
+            unsigned char sbox[256];
+            int sizeKey,prgaIndexA,prgaIndexB;
+    };
+
+
+
+#endif /* ARC4_H_ */
