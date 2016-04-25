38f86992b49f6125abc12dc894639c7d57a2a4af
fabio <blackjackdevel@gmail.com>
2016-04-25 10:36:14 -0300

    Added examples

---
+++ examples/testRC4.cpp
@@ -0,0 +1,34 @@
+#include <ARC4.h>
+#include <iostream>
+#include <string>
+#include <malloc.h>
+#include <string.h>
+
+using std::string;
+using std::cout;
+
+int main ()
+{
+    string someKey  = "I_AM_A_KEY";
+    string toEncrypt = "I am a plain text string";
+    ARC4 rc4;
+    rc4.set_key((unsigned char*) someKey.c_str(),someKey.length());
+    char * enc=  (char *) malloc(toEncrypt.size()+1);
+    char * dec=  (char *) malloc(toEncrypt.size()+1);
+
+    rc4.encrypt_char((char *)  toEncrypt.c_str(), enc, toEncrypt.length());
+    cout<<" Encrypted result \n";
+    cout << enc << "\n";
+
+    rc4.set_key((unsigned char*) someKey.c_str(),someKey.length());
+    rc4.encrypt_char(enc , dec, toEncrypt.length());
+
+    cout<<" Decrypted result \n";
+    cout << dec << "\n";
+    if(strcmp(dec, toEncrypt.c_str()) ==0){
+        cout<<"Enctyption successfull!\n";
+    }
+    else{
+        cout<<"Something is wrong on the encriptyon!\n";
+    }
+}
